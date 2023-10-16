c9703ab40a08cf929f792958eed209ed0a2ac608
Evgenii Klinnikov <mostransformer@gmail.com>
2023-10-16 22:42:07 +0400

    zenrows proxy

--- screener/src/requests.rs
+++ screener/src/requests.rs
@@ -1,6 +1,5 @@
-use curl::easy::Easy;
+use reqwest;
 use serde::Deserialize;
-use serde_json;
 use urlencoding::encode;
 
 #[derive(Deserialize, Debug)]
@@ -34,31 +33,34 @@ pub struct ItemMetadata {
 pub async fn get_rental(query: &String) -> Option<ItemsResponse> {
     println!("Getting rentals for '{}'", query);
 
-    let mut json = Vec::new();
-
-    let mut easy = Easy::new();
-    easy.proxy_username(&dotenv::var("PROXY_USER").expect("PROXY_USER must be set")).unwrap();
-    easy.proxy_password(&dotenv::var("PROXY_PASSWORD").expect("PROXY_PASSWORD must be set")).unwrap();
-    easy.proxy(&dotenv::var("PROXY_URL").expect("PROXY_URL must be set")).unwrap();
-    easy.useragent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36").unwrap();
-    easy.proxy_port(10000).unwrap();
-    easy.get(true).unwrap();
+    let proxy = reqwest::Proxy::https(format!(
+        "http://{}:{}@{}",
+        &dotenv::var("PROXY_USER").expect("PROXY_USER must be set"),
+        &dotenv::var("PROXY_PASSWORD").expect("PROXY_PASSWORD must be set"),
+        &dotenv::var("PROXY_URL").expect("PROXY_URL must be set")
+    ))
+    .unwrap();
+    let client = reqwest::ClientBuilder::new()
+        .proxy(proxy)
+        .danger_accept_invalid_hostnames(true)
+        .danger_accept_invalid_certs(true)
+        .build()
+        .unwrap();
 
     let encoded_query = encode(query);
-    easy.url(&format!("https://api.openloot.com/v2/market/rentals?gameId=56a149cf-f146-487a-8a1c-58dc9ff3a15c&page=1&pageSize=100&sort=price%3Aasc&q={}", encoded_query)).unwrap();
+    let request_url = format!("https://api.openloot.com/v2/market/rentals?gameId=56a149cf-f146-487a-8a1c-58dc9ff3a15c&page=1&pageSize=100&sort=price%3Aasc&q={}", encoded_query);
 
-    {
-        let mut transfer = easy.transfer();
-        transfer
-            .write_function(|data| {
-                json.extend_from_slice(data);
-                Ok(data.len())
-            })
-            .unwrap();
-        transfer.perform().unwrap();
-    }
+    let req = client.get(request_url);
 
-    let result: ItemsResponse = match serde_json::from_slice(&json) {
+    let res = match req.send().await {
+        Ok(result) => result,
+        Err(err) => {
+            eprintln!("Error getting items: {}", err);
+            return None;
+        }
+    };
+
+    let result: ItemsResponse = match res.json().await {
         Ok(res) => res,
         Err(err) => {
             eprintln!("Error parsing ItemsResponse: {}", err);
